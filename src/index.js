8ab13f55624f0afd575c28a0c935275767de5f95
Marcos Sandoval <76020778+marcos1394@users.noreply.github.com>
2025-08-11 21:04:44 -0700

    Update index.js
    index con el contexto nuevo de auth

--- src/index.js
+++ src/index.js
@@ -1,18 +1,17 @@
-// src/index.js
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './styles/index.css'; // La ruta a tus estilos principales
+import './styles/index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { AuthProvider } from './context/AuthContext'; // <-- IMPORTANTE
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <AuthProvider> {/* <-- ENVOLVEMOS LA APP */}
+      <App />
+    </AuthProvider>
   </React.StrictMode>
 );
 
-// Si quieres empezar a medir el rendimiento en tu app, puedes pasar una función
-// para registrar los resultados (por ejemplo: reportWebVitals(console.log))
-// o enviar a un punto de análisis. Más información: https://bit.ly/CRA-vitals
 reportWebVitals();
