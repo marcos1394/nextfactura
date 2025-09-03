c17e869fd26fbe38b5cfceea98c45f7024b23921
marcos sandoval <ingsandoval1394@gmail.com>
2025-09-02 20:11:21 -0600

    correccion de llamada a planes

--- src/pages/PlanSelection.js
+++ src/pages/PlanSelection.js
@@ -1,5 +1,5 @@
 // src/pages/PlanSelection.js
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { motion, AnimatePresence } from 'framer-motion';
 import { useThemeContext } from '../context/ThemeContext';
